cbe3c5818458b7acc38a4881b3c48fc5f275aa86
Chris P <j393554@tafe.wa.edu.au>
2019-09-26 10:40:46 +0800

    Created simple mock tests (not using actual unit testing capabilities) to Manager and Employee classes

--- EmployeeManagerCSharp/Program.cs
+++ EmployeeManagerCSharp/Program.cs
@@ -10,6 +10,16 @@ namespace EmployeeManagerCSharp
   {
     static void Main(string[] args)
     {
+      Manager emp1 = new Manager(123, "Bob");
+
+      Employee emp2 = new Employee(234, "Zac");
+      Employee emp3 = new Employee(345, "Deb");
+
+      emp1.AddEmployee(emp2);
+      emp1.AddEmployee(emp3);
+
+      Console.WriteLine(emp1.PrintDetails());
+      Console.ReadLine();
     }
   }
 }
